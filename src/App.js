539c7178fb11c053736acc194b520a954cedb334
Abdul Razak <moabdulrazak17@gmail.com>
2020-02-19 14:29:48 +0000

    adding SearchBar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import { Grid } from '@material-ui/core';
+
+import SearchBar from './components/SearchBar';
+import VideoDetail from './components/VideoDetail';
+// import { VideoList } from './components/VideoList';
+
+
+import youtube from './api/youtube';
+
 class App extends React.Component {
     render () {
         return (
-            <h1>Youtube Clone App</h1>
+            <Grid justify="center" container spacing={16}>
+	            <Grid item xs={12}>
+		            <Grid container spacing={16}>
+			            <Grid item xs={12}>
+			            	<SearchBar />
+			            </Grid>
+
+			            <Grid item xs={8}>
+			            	<VideoDetail />
+			            </Grid>
+
+		            	<Grid item xs={4}>
+			            	{/* VIDEO LIST */}
+		            	</Grid>
+		            </Grid>
+	            </Grid>
+            </Grid>
         )
     }
 }
