9ab600271ea6c4e703d5cb9385c5556b3064f6c1
Abdul Razak <moabdulrazak17@gmail.com>
2020-02-20 12:59:44 +0000

    setting up video components

--- src/App.js
+++ src/App.js
@@ -2,30 +2,49 @@ import React from 'react';
 
 import { Grid } from '@material-ui/core';
 
-import SearchBar from './components/SearchBar';
-import VideoDetail from './components/VideoDetail';
+import { SearchBar, VideoDetail, VideoList } from './components';
 // import { VideoList } from './components/VideoList';
 
 
 import youtube from './api/youtube';
 
+const API_KEY =`${process.env.REACT_APP_API_KEY_YT}`
+
 class App extends React.Component {
+	state = {
+		video: [],
+		selectedVideo: null,
+	}
+
+	handleSubmit = async (searchTerm) => {
+		const response = await youtube.get('search', {
+			params: {
+				part: 'snippet',
+				maxResults: 5,
+				key: API_KEY,
+				q: searchTerm,
+			}
+		
+		});
+
+		this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+	}
     render () {
+		const { selectedVideo } = this.state;
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
 	            <Grid item xs={12}>
-		            <Grid container spacing={16}>
+		            <Grid container spacing={10}>
 			            <Grid item xs={12}>
-			            	<SearchBar />
+			            	<SearchBar onFormSubmit={this.handleSubmit} />
 			            </Grid>
 
 			            <Grid item xs={8}>
-			            	<VideoDetail />
+			            	<VideoDetail video={selectedVideo}/>
 			            </Grid>
 
 		            	<Grid item xs={4}>
-			            	{/* VIDEO LIST */}
-		            	</Grid>
+			            	<VideoList />		            	</Grid>
 		            </Grid>
 	            </Grid>
             </Grid>
