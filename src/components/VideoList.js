2ead65f781915da223e6d0a96f3d2ba5d7fedefd
Abdul Razak <moabdulrazak17@gmail.com>
2020-02-20 12:58:19 +0000

    initial build of video ui

---
+++ src/components/VideoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+const VideoList = () => {
+    return (
+        <h1>Video List</h1>
+    )
+}
+
+export default VideoList
+
